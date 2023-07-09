250a14949334fa4552902f4e29f88c0480b2f3ee
ChiomaNwade <113738097+ChiomaNwade@users.noreply.github.com>
2023-07-09 21:06:29 +0100

    Update test_shop.py

--- test_shop.py
+++ test_shop.py
@@ -1,5 +1,3 @@
-#TASK 4
-# Unit Tests
 import unittest
 from shop import Shop
 
@@ -8,29 +6,31 @@ class ShopTestCase(unittest.TestCase):
         self.shop = Shop()
 
     def test_valid_purchase(self):
+        """Test a valid purchase by checking if the available money is updated correctly."""
         self.shop.process_purchase('item1')
         self.assertEqual(self.shop.available_money, 50)
 
     def test_invalid_item(self):
+        """Test purchasing an invalid item raises a ValueError."""
         with self.assertRaises(ValueError):
             self.shop.process_purchase('item5')
 
     def test_insufficient_funds(self):
+        """Test purchasing an item with insufficient funds raises a ValueError."""
         with self.assertRaises(ValueError):
             self.shop.process_purchase('item4')
 
     def test_ask_for_more_money(self):
+        """Test adding more money to the available funds."""
         self.shop.ask_for_more_money()
         self.assertEqual(self.shop.available_money, 100)
 
     def test_maximum_purchase_attempts(self):
+        """Test reaching the maximum purchase attempts and check if a ValueError is raised."""
         self.shop.purchase_attempts = 3
         with self.assertRaises(ValueError):
-            self.shop.process_purchase('item1')
+            self.shop.run_shop()
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
